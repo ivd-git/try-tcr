4ac33be6fbe52d6a097b7b27a7e65a16910c250b
Ingmar van Dijk <ingmar.van.dijk@philips.com>
2018-11-13 11:27:14 +0100

    Updated to JUNit 4

--- try-it/src/test/java/com/philips/tcs/AppTest.java
+++ try-it/src/test/java/com/philips/tcs/AppTest.java
@@ -1,42 +1,22 @@
 package com.philips.tcs;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Test
+    public void firstTest() {
+        assertTrue(true);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-
+    @Test
     public void testPass() {
-       assertTrue( true );
+        assertTrue(true);
     }
 }
